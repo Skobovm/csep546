23bb18899aed0cf6469043d8b86b50547fe196b1
Mikhail Skobov <skobovm@gmail.com>
2017-10-17 18:52:54 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+import scipy
+
+
+def main():
+    logging.info("---Starting application---")
+
+
+    pass
+
+
+if __name__ == '__main__':
+    # Set logging level to 0, so info and debug gets output
+    logging.root.level = 0
+    sys.exit(main())
